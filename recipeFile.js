4a9d3184b1bef23262ce913c1b08f4b4e6896957
UltanBlack <159574573+UltanBlack@users.noreply.github.com>
2024-04-30 14:25:29 -0400

    File for all recipes

---
+++ recipeFile.js
@@ -0,0 +1,194 @@
+const recipe1 = {
+    title: "Pasta Carbonara",
+    image: "https://kirbiecravings.com/wp-content/uploads/2020/02/spaghetti-carbonara-1.jpg",
+    description: "Classic Italian pasta dish",
+    instructions: "1. Cook pasta. 2. Fry bacon. 3. Mix eggs and cheese. 4. Combine all ingredients.",
+    ingredients: ["8 oz spaghetti", "4 slices bacon", "2 eggs", "1/2 cup grated Parmesan cheese", "Salt and pepper to taste"]
+};
+
+const recipe2 = {
+    title: "Shrimp Po' Boys",
+    image: "https://www.melissassouthernstylekitchen.com/wp-content/uploads/2021/02/Cajun-Shrimp-Poboys.jpg",
+    description: "A savory seafood dish",
+    instructions: "1. Preheat stove. 2. Combine all ingredients. 3. Place in deep fryer. 4. Prepare sandwich as desired.",
+    ingredients: ["vegetable oil", "1 large egg", "1 cup of milk", "1 1/2 pounds of large shrimp", "salt", "1/2 cup flour", "1 cup of bread crumbs", "2 tbsp seafood seasoning", "soft club rolls", "1 tomato", "1 head lettuce", "1 lemon", "1/4 cup sweet pickle relish", "1/2 cup spicy mustard", "hot sauce"]
+};
+
+const recipe3 = {
+    title: "Caesar Salad",
+    image: "https://wallpapercave.com/wp/wp3131444.jpg",
+    description: "A classic vegetable",
+    instructions: "1. Prepare the croutons. 2. Make the dressing. 3. Chop up lettuce. 4. Combine all ingredients.",
+    ingredients: ["1/2 cup vegetable oil", "2 cups of loosely torn country bread", "1/4 cup grated Parmigiano-Reggiano", "2 tbsp minced parsley", "2 tbsp roasted garlic", "2 tbsp sherry vinegar", "1 tbsp Dijon mustard", "6 white anchovies", "1 egg yolk", "1 1/4 cups canola oil", "1 head Romaine lettuce"]
+};
+
+const recipe4 = {
+    title: "Blueberry Pancakes",
+    image: "https://th.bing.com/th/id/R.8cafa85ae05c7ae78633ab2c719ae0e5?rik=q939oR0T1PEb0w&pid=ImgRaw&r=0g",
+    description: "The best breakfast",
+    instructions: "1. Mix the ingredients. 2. Place batter on skillet. 3. Flip until fully cooked.",
+    ingredients: ["1 1/4 cups flour", "1 tbsp baking powder", "1 1/4 tsps white sugar", "1/2 tsp salt", "1 cup milk", "1 egg", "1/2 tbsp melted butter", "1/2 cup frozen blueberries"]
+};
+
+const recipe5 = {
+    title: "French Fries",
+    image: "https://tastylicious.com/wp-content/uploads/2022/10/french-fries-with-mayo-and-ketchup.jpg",
+    description: "A classic sidepiece",
+    instructions: "1. Chop potato into strips. 2. Deep fry strips in hot oil. 3. Season as needed.",
+    ingredients: ["2 cups vegetable oil", "1 tbsp salt", "1 large potato"]
+};
+
+const recipe6 = {
+    title: "Lobster Bisque",
+    image: "https://www.sweetandsavorybyshinee.com/wp-content/uploads/2021/06/Lobster-Bisque-2-1024x1536.jpg",
+    description: "For a classy night in",
+    instructions: "1. Cook vegetables in butter until tender. 2. Add the broth and season as needed. 3. Add the lobster then cover. 4. Stir in the half-and-half and wine before letting simmer",
+    ingredients: ["3 tbsp butter", "2 tbsp chopped onion", "2 tbsp chopped carrot", "2 tbsp chopped celery", "1/4 cup of chopped mushrooms", "14.5 oz chicken broth", "1/2 cup dry white wine", "1/2 lb lobster", "1 1/2 cup half-and-half"]
+};
+
+const recipe7 = {
+    title: "Cod with Lemon Aioli",
+    image: "https://www.acouplecooks.com/wp-content/uploads/2020/09/Baked-Cod-059.jpg",
+    description: "For a classy night in",
+    instructions: "1. Stir Stir mayonnaise, lemon zest, lemon juice, and grated garlic together then season with salt, pepper, and milk. 2. Pat fish dry with paper towel, heat olive oil and butter in a sillet over medium high. Sprinkle both sides of codwith Italian seasoning, salt, and pepper. 3. Add cod to skillet, sear until golden brown on both sides and fish flakes, 3-4 minutes per side.",
+    ingredients: ["2 tbsp mayonnaise", "1 tsp milk", "1 tsp lemon juice", "1 tsp grated garlic", "1/2 tsp lemon zest", "4 (6 oz) cod filets", "1 tbsp olive oil", "1 tbsp unsalted butter", "1 tsp Italian seasoning"]
+};
+
+const recipe8 = {
+    title: "Stuffed Bell Peppers",
+    image: "https://10xfitness.co.uk/wp-content/uploads/2019/01/delish-classic-stuffed-peppers-horizontal-1538065876-scaled.jpg",
+    description: "A healthy alternative",
+    instructions: "1. Precook rice and ground beef. 2. Combine ingredients in bowl 3. Stuff peppers. 4. Bake until ready.",
+    ingredients: ["1/2 cup of white rice", "6 bell peppers (remove core)", "1 chopped medium yellow onion", "2 tbsp olive oil", "3 chopped cloves of garlic", "2 tbsp tomato paste", "1 lb ground beef", "1 1/2 tsp dried oregano", "1 cup shredded cheese", "14.5 oz diced tomatoes"]
+};
+
+const recipe9 = {
+    title: "Mac 'n Cheese",
+    image: "https://cdn.loveandlemons.com/wp-content/uploads/2013/01/vegan-mac-and-cheese.jpg",
+    description: "An easy classic",
+    instructions: "1. Cook macaroni until soft. 2. Melt and season the cheese 3. Mix until fully combined.",
+    ingredients: ["1 1/2 cups elbow macaroni", "3 tbsp butter", "2 tbsp flour", "1/2 tsp salt", "1/8 tsp ground black pepper", "1 1/2 cups milk", "3/4 cup half and half", "2 1/2 cups shredded cheese"]
+};
+
+const recipe10 = {
+    title: "Acai Bowl",
+    image: "https://grabsomejoy.com/wp-content/uploads/2019/05/Berry-Fruity-Acai-Bowel-5.jpg",
+    description: "A healthy snack",
+    instructions: "1. Blend yogurt, acai puree, frozen berries, and juice in a blender until smooth. 2. Pour mix into bowl before placing toppings as desired.",
+    ingredients: ["1 package of acai puree", "1 cup frozen berries", "1/2 frozen banana", "1 cup of yogurt", "1/2 cup of fruit juice", "granola and berry toppings", "honey"]
+};
+
+const recipe11 = {
+    title: "Fried Rice",
+    image: "https://cff2.earth.com/uploads/2018/11/19174121/The-science-behind-cooking-perfectly-fried-rice-.jpg",
+    description: "Meals made easy",
+    instructions: "1. Cook rice. 2. Mix together all ingredients on the stove 3. Mix and allow to cook until soft.",
+    ingredients: ["3 cups rice", "2 large eggs", "2 tbsp vegetable oil", "1/2 cup frozen peas", "1 tbsp soy sauce", "tsp sesame oil", "1 clove minced garlic", "2/3 cup chopped carrots"]
+};
+
+const recipe12 = {
+    title: "BLT Sandwich",
+    image: "https://thefoodcharlatan.com/wp-content/uploads/2012/05/How-to-Make-the-Best-BLT-8.jpg",
+    description: "Lunch made easy",
+    instructions: "1. Prepare ingredients. 2. Allow bacon to sizzle until fully cooked. 3. Assemble sandwich.",
+    ingredients: ["2 bread slices", "3 leaves of lettuce", "1 tomato", "1 tbsp mayonaise", "1/4 lb bacon"]
+    
+};
+
+const recipe13 = {
+    title: "Potato Gnocchi",
+    image: "https://www.eatwell101.com/wp-content/uploads/2018/05/Potato-Gnocchi-in-Bacon-Spinach-Cream-Sauce.jpg",
+    description: "Classic Italian pasta dish",
+    instructions: "1. Remove skin and allow potatoes to boil before mashing. 2. Mix together flour, salt, potatos, and egg until a soft dough begins to form. 3. Cut dough into small pieces then allow to sit. 4. Boil gnocchi until they float to the top. 5. Season as needed.",
+    ingredients: ["1/2 lb Russet potatos", "2 cups flour", "2 tsp salt", "1 large egg"]
+};
+
+const recipe14 = {
+    title: "Beef Stroganoff",
+    image: "https://hips.hearstapps.com/hmg-prod/images/beef-stroganoff-horizontal-1539197158.jpg",
+    description: "A British classic",
+    instructions: "1. Saute ground beef and season as needed. 2. Cook egg noodles. 3. Stir soup and garlic powder into the beef. 4. Remove the beef from heat then stir in sour cream and egg noodles.",
+    ingredients: ["8 oz egg noodles", "1 lb ground beef", "10.5 oz condensed cream of mushroom soup", "1 btbsp garlic powder", "1/2 cup sour cream", "Salt and pepper to taste"]
+};
+
+const recipe15 = {
+    title: "Chicken Pot Pie",
+    image: "https://www.kitchensanctuary.com/wp-content/uploads/2016/01/Creamy-Chicken-Pie-square1.jpg",
+    description: "Comfort food?",
+    instructions: "1. Precook chicken. 2. Prepare pie crust. 3. Mix together ingredients. 4. Pour mix into pie crust before cooking until ready.",
+    ingredients: ["1 package puff pastry", "2 lbs chicken breast", "1 tsp salt", "1 tsp pepper", "4 tbsp butter", "1 large chopped onion", "2 minced garlic cloves", "1 tsp minced thyme", "1 tsp mince sage", "1/2 cup flour", "1 1/2 cups chicken broth", "1 ", "1/4 cup half and half cream", "2 cups mixed vegetables", "1 larg egg yolk"]
+};
+
+const recipe16 = {
+    title: "Strawberry Salad",
+    image: "https://www.baconismagic.ca/wp-content/uploads/2021/07/strawberry-salad-1152x1536.jpg",
+    description: "An alternative twist to regular salad",
+    instructions: "1. Prepare fruits and vegetables. 2. Caramelize almonds. 3. Combine all ingredients. 4. Add dressing as needed.",
+    ingredients: ["1/4 cup sugar", "2 cups halved fresh strawberries", "1/3 cup slivered almonds", "1 head romaine lettuce (torn)", "1 halved small onion"]
+};
+
+const recipe17 = {
+    title: "Asian Turkey Lettuce Cups",
+    image: "https://www.eatyourselfskinny.com/wp-content/uploads/2023/01/lettuce-wraps-1-scaled.jpg",
+    description: "Simple but filling",
+    instructions: "1. Cook turkey. 2. Whisk soy sauce, sugar, and sesame oil then add to turkey. 3. Mix together remaining ingredients. 4. Serve in lettuce leaves.",
+    ingredients: ["2 cups cooked brown rice", "3 tbsp soy sauce", "2 tsp white sugar", "2 tsp sesame oil", "1 lb ground turkey", "1 celery rib (chopped)", "1 minced gingerroot", "1 minced garlic clove", "8 oz water chestnuts", "1 medium carrot (shredded)", "1 head of lettuce"]
+};
+
+const recipe18 = {
+    title: "Baked Parmesan Broccoli",
+    image: "https://themom100.com/wp-content/uploads/2019/02/parmesan-roasted-broccoli-250.jpg",
+    description: "A yummy twist to broccoli",
+    instructions: "1. Steam broccoli until tender. 2. Mix together butter, onions, garlic, and flour, then add milk gradually. 3. Allow egg yolk to cook in mixture. 4. Stir mix with broccoli and add cheese and seasoning as needed.",
+    ingredients: ["4 broccoli bunches", "6 tbsp butter", "1 small chopped onion", "1 minced garlic clove", "1/4 cup flour", "2 cups milk", "1 large egg yolk", "1 cup grated Parmesan cheese", "1/2 tsp salt", "1/8 tsp pepper", "1/2 cup seasoned bread crumbs"]
+};
+
+const recipe19 = {
+    title: "Garlic Shrimp Pasta",
+    image: "https://th.bing.com/th/id/OIP.jpvZ2yWWjhpzi4-_5raQSAHaE8?rs=1&pid=ImgDetMain",
+    description: "A gourment meal at home",
+    instructions: "1. Cook pasta until softened. 2. Cook shrimp with butter, salt, and red pepper until pink. 3. Drain pasta then add butter and garlic. 4. Combine shrimp with pasta then top with parsley, garlic, and cheese",
+    ingredients: ["8 oz pasta", "4 tbsp butter", "1 lb large shrimp (peeled)", "1/4 tsp salt", "1/2 tsp crushed red pepper", "4 minced garlic cloves", "2 tbsp lemon juice", "3 tbsp grated Parmesan", "2 tbsp chropped parsley"]
+};
+
+const recipe20 = {
+    title: "Sheek Kebabs",
+    image: "https://i.pinimg.com/originals/3f/da/3e/3fda3e5cf7b10e00f1a387c2a8c95266.jpg",
+    description: "Tasteful",
+    instructions: "1. Mix thoroughly the ground beef with onion, garlic, cilantro, garam masala, cumin powder, coriander powder, chili powder, black pepper, and salt. 2. Divide the mix into equal portions before placing on the skewers. 3. Brush kebabs with oil. 4. Cook until ready.",
+    ingredients: ["1 lb ground beef", "1 small chopped onion", "2 minced garlic cloves", "2 tbsp chopped cilantro", "1 tsp garam masala", "1 tsp cumin powder", "1/2 tsp coriander powder", "1/2 tsp chili powder", "1/4 tsp black pepper", "2 tbsp vegetable oil"]
+};
+
+const recipe21 = {
+    title: "Baked Chicken Breasts",
+    image: "https://www.eatwell101.com/wp-content/uploads/2022/07/Oven-Baked-Chicken-Breasts-1.jpg",
+    description: "A healthy alternative",
+    instructions: "1. Rub the chicken breast with olive oil then season both sides with salt and Creole seasoning. 2. Cook until ready 3. Pour chicken broth into the pan.",
+    ingredients: ["4 skinless, boneless chicken breast halves", "2 tbsp olive oil", "1/2 tsp salt", "1 pinch Creole seasoning", "1 tbsp chicken broth", "Salt and pepper to taste"]
+};
+
+const recipe22 = {
+    title: "Classic Sushi",
+    image: "https://images.squarespace-cdn.com/content/v1/5280c154e4b05645e635cf76/1559759361645-PSFYJGHZJKDOUCK43NE4/ke17ZwdGBToddI8pDm48kCHChmuivJZ1Va5ov3ZJeg17gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0ouw-8l5B_J38LMU7OZFvYcSGirBhY_3j1yQtntvGS73bypqQ-qjSV5umPUlGbQFAw/This+easy+vegan+sushi+recipe+can+be+pulled+together+in+less+than+30+minutes.+It's+delicious%2C+healthy%2C+and+easy+to+make.",
+    description: "Restaurant Quality Dishes",
+    instructions: "1. Heat and stir together the rice vinegar, sugar, and salt. 2. Pour mix into rice 3. With damp hands, spread rice evenly ontop of seaweed before placing fish and vegetables. 4. Role tightly then slice.",
+    ingredients: ["2 cups cooked sushi rice", "1/3 cup rice vinegar", "2 tbsp sugar", "1 tsp salt", "4 sheets dried seaweed (nori)", "1/2 lb fresh fish", "1 peeled and cut cucumber", "1 peeled and sliced avocado"]
+};
+
+const recipe23 = {
+    title: "Spiced Pumpkin Hummus",
+    image: "https://images-ext-1.discordapp.net/external/4WMVnqBAOfnbQNKlp1afgJUocUUZSXwuuxesf726kEU/https/thecrumbykitchen.com/wp-content/uploads/2017/09/Roasted-Garlic-Pumpkin-Hummus-1.jpg?format=webp&width=396&height=593",
+    description: "Enjoy with pita chips",
+    instructions: "1. Blend all ingredients together until smooth. 2. Place ganishes as desired.",
+    ingredients: ["15 oz canned chickpeas", "1 cup pumpkin puree", "2 cloves minced garlic", "2 tbsp tahini", "2 tbsp olive oill", "1 tsp lemon juice", "1/2 tsp ground cumin", "1/2 tsp smoked paprika", "1/4 tsp cinnamon"]
+};
+
+const recipe24 = {
+    title: "Wild Mushroom and Gruyere Tart",
+    image: "https://iloveimportedcheese.com/wp-content/uploads/2022/10/Gruyere-Mushroom-Tart_sm-1000x1000.jpg",
+    description: "Another pie dinner",
+    instructions: "1. Prepare pie crust. 2. Cook the onions and mushrooms until tender (season as needed). 3. Mix together eggs, cream, and cheese. 4. Combine both mixes in the pie crust. 5. Bake until ready.",
+    ingredients: ["1 can of premade pie crust", "2 cups sliced mushrooms", "1 thinly sliced onion", "1 cup grated Gruyere cheese", "1/2 cup heavy cream", "2 eggs", "2 tbsp fresh thyme", "2 tbsp butter", "Salt and pepper to taste"]
+};
+
+const recipes = [recipe1, recipe2, recipe3, recipe4, recipe5, recipe6, recipe7, recipe8, recipe9, recipe10, recipe11, recipe12, recipe13, recipe14, recipe15, recipe16, recipe17, recipe18, recipe19, recipe20, recipe21, recipe22, recipe23, recipe24];
